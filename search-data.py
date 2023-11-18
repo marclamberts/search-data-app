769b60626cdff8e48527cc202260109807437025
Marc Lamberts <43936834+marclamberts@users.noreply.github.com>
2023-11-18 19:49:07 +0100

    Update search-data.py

--- search-data.py
+++ search-data.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 from scipy import stats  # Import the stats module for percentileofscore
 import math
 
